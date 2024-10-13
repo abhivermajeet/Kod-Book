6949721d3384a11943d9aaefe5b2c78f49e80376
HP <HP@DESKTOP-2CQI695>
2024-10-13 21:47:19 +0530

    My first commit

---
+++ com.kodbook/src/main/java/com/kodbook/services/PostServiceImplementation.java
@@ -0,0 +1,34 @@
+package com.kodbook.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kodbook.entities.Post;
+import com.kodbook.repositories.PostRepository;
+
+@Service
+public class PostServiceImplementation  
+                      implements PostService{
+	
+	@Autowired
+	PostRepository repo;
+
+	@Override
+	public void createPost(Post post) {
+		repo.save(post);
+		
+	}
+
+	@Override
+	public List<Post> getAllPosts() {
+		return repo.findAll();	
+	}
+
+	@Override
+	public List<Post> fetchAllPosts() {
+		return repo.findAll() ;
+	}
+
+}
